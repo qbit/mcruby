8b346d529864fae2af7037f2a63249c9ff9c5e38
Aaron Bieber <aaron@bolddaemon.com>
2018-08-23 17:41:18 -0600

    less !!

--- plugins/Hello.rb
+++ plugins/Hello.rb
@@ -3,13 +3,13 @@ require './plugins/plugins.rb'
 # Respond to hello
 class Hello < Plugin
   def response(_to, from, msg)
-    "Hello there, #{from}!!!" if /^hi$/i =~ msg
+    "Hello there, #{from}!" if /^hi$/i =~ msg
   end
 
   def test
     resp = response('test', 'test', 'Hi')
     resp = resp.chomp
 
-    resp == 'Hello there, test!!!'
+    resp == 'Hello there, test!'
   end
 end

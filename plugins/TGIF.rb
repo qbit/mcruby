ca6650f01e95d63156224bb23f9a5647ef93d005
Aaron Bieber <aaron@bolddaemon.com>
2018-08-24 12:34:16 -0600

    tgif!

---
+++ plugins/TGIF.rb
@@ -0,0 +1,21 @@
+require './plugins/plugins.rb'
+
+# FRIDAY?!
+class TGIF < Plugin
+  @@friday = <<EOF
+ _____ ____  ___ ____    _ __   ___ 
+|  ___|  _ \|_ _|  _ \  / \\ \ / / |
+| |_  | |_) || || | | |/ _ \\ V /| |
+|  _| |  _ < | || |_| / ___ \| | |_|
+|_|   |_| \_\___|____/_/   \_\_| (_)
+EOF
+  def response(_to, _from, msg)
+    time = Time.now
+    return @@friday if time.wday == 5 && msg =~ %r{tgif}i
+    return ':(' if time.wday != 5
+  end
+
+  def test
+    true
+  end
+end

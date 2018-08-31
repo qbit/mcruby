ff29fdec2c10efe82174747ed12fb90a0e0d3379
Aaron Bieber <aaron@bolddaemon.com>
2018-08-31 12:49:11 -0600

    named heredoc

--- plugins/TGIF.rb
+++ plugins/TGIF.rb
@@ -2,13 +2,13 @@ require './plugins/plugins.rb'
 
 # FRIDAY?!
 class TGIF < Plugin
-  @@friday = <<EOF
+  @@friday = <<FRIDAY
  _____ ____  ___ ____    _ __   ___
 |  ___|  _ \|_ _|  _ \  / \\ \ / / |
 | |_  | |_) || || | | |/ _ \\ V /| |
 |  _| |  _ < | || |_| / ___ \| | |_|
 |_|   |_| \_\___|____/_/   \_\_| (_)
-EOF
+FRIDAY
   def response(_to, _from, msg)
     time = Time.now
     return @@friday if time.wday == 5 && msg =~ %r{tgif}i
